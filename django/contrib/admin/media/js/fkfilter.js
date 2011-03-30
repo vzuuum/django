b3846e4c48d44f5034c6bd3821bbb485d4f3b22b
Sean Bleier <sebleier@gmail.com>
2011-03-29 23:46:44 -0700

    Trying to be clever can be dumb.  The filter select should work correcly now.

--- django/contrib/admin/media/js/fkfilter.js
+++ django/contrib/admin/media/js/fkfilter.js
@@ -1,8 +1,7 @@
 (function($) {
     $.fn.fk_filter = function(verbose_name) {
         return this.each(function() {
-            var excluded;;
-            var j = 0;
+            var stash = [];
             $(this).attr('size', 2); // Force a multi-row <select>
             $(this).find('option[value=""]').remove(); // Remove the '-----'
 
@@ -28,11 +27,7 @@
             }).insertBefore(this);
 
             var select = $(this);
-            var original = select.clone();
-
             var filter = $('#' + $(this).attr('id') + '_input');
-            var excluded = [];
-            var lastPatternLength = 0;
 
             filter.bind('keyup.fkfilter', function(evt) {
                 /*
@@ -40,44 +35,45 @@
 
                     * Detach the select from the DOM so each change doesn't
                       trigger the browser to re-render.
-                    * If the pattern is longer than the previous pattern, then
-                      we can skip any excluded select options and search the
-                      included options for pattern matches.
-                    * If the pattern is shorter, then we're forced to check
-                      excluded select options for matches.
-                    * ``matched`` is incremented whenever an excluded select
+                    * Iterate through the stash list for matches
+                    * ``matched`` is incremented whenever an stashed select
                       option is matched so that we don't have to search
                       recently appended options twice.
-                    * Note the length of the pattern in ``lastPatternLength``.
-                    * Append the select back into the DOM for rendering.
+                    * Iterate through select's options and stash mismatched
+                      options
+                    * Attach the select back into the DOM for rendering.
                 */
                 var i, size, option, options;
                 var pattern = filter.val();
                 var parent = select.parent();
                 var matched = 0;
+
+                // detach from DOM
                 select.detach();
-                if (lastPatternLength > pattern.length) {
-                    size = excluded.length;
-                    for (i = 0; i < size; i++) {
-                        option = excluded[i];
-                        if (option.text().indexOf(pattern) !== -1) {
-                            select.append(option);
-                            matched++;
-                        }
+
+                // Iterate through the excluded list for matches
+                size = stash.length;
+                for (i = 0; i < size; i++) {
+                    if (stash[i].text.indexOf(pattern) !== -1) {
+                        select.append(stash.splice(i--, 1));
+                        matched++;
+                        size--;
                     }
                 }
+
+                // Iterate through existing options for matches
                 options = select.children();
                 size = options.length - matched;
                 for(i = 0; i < size; i++) {
                     if (options[i].text.indexOf(pattern) === -1) {
                         option = $(options[i]).detach();
-                        excluded.push(option);
+                        stash.push(option[0]);
                     }
                 }
-                lastPatternLength = pattern.length;
+
+                // Attach back onto the DOM
                 parent.append(select);
             });
         });
     };
 })(django.jQuery);
-
