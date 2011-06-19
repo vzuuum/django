f55258c9c22acbf1e3a536e0b27dc1c7f546a6bf
Sean Bleier <sebleier@gmail.com>
2011-06-19 15:31:48 -0700

    Serving static files first

--- django/test/javascript/urls.py
+++ django/test/javascript/urls.py
@@ -4,8 +4,9 @@ from django.test.javascript.views import JavascriptTestOverview, \
     JavascriptTest, static_path
 
 
-urlpatterns = patterns('',
+urlpatterns =  staticfiles_urlpatterns() + patterns('',
     url('^test/(?P<path>.*)$', 'django.views.static.serve', {'document_root': static_path}, name='javascript-static'),
     url('^$', JavascriptTestOverview.as_view(), name='javascript-test-overview'),
     url('^(?P<label>[\w]+)/(?P<path>.*)$', JavascriptTest.as_view(), name='javascript-test-runner'),
-) + staticfiles_urlpatterns()
+)
+
